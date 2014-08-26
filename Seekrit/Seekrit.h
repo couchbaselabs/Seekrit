5b3ebb0d4832e6acd0585f08e03e20ca95271958
Jens Alfke <jens@couchbase.com>
2014-08-25 18:30:39 -0700

    Initial Commit

---
+++ Seekrit/Seekrit.h
@@ -0,0 +1,13 @@
+//
+//  Seekrit.h
+//  Seekrit
+//
+//  Created by Jens Alfke on 8/25/14.
+//  Copyright (c) 2014 Couchbase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Seekrit : NSObject
+
+@end
---
+++ Seekrit/Seekrit.m
@@ -0,0 +1,13 @@
+//
+//  Seekrit.m
+//  Seekrit
+//
+//  Created by Jens Alfke on 8/25/14.
+//  Copyright (c) 2014 Couchbase. All rights reserved.
+//
+
+#import "Seekrit.h"
+
+@implementation Seekrit
+
+@end
