204d2915a4c287cfc01206e2f5541ce7de655183
Jens Alfke <jens@couchbase.com>
2015-03-30 12:01:33 -0700

    Show public key mnemonic below QR code

--- Seekrit iOS Demo/FirstViewController.m
+++ Seekrit iOS Demo/FirstViewController.m
@@ -10,12 +10,14 @@
 #import "AppDelegate.h"
 #import "CBKey.h"
 #import "SignedJSON.h"
+#import "NSData+Mnemonic.h"
 #import "CBQRCode.h"
 
 
 @interface FirstViewController ()
 {
     IBOutlet UIImageView* qrDisplayView;
+    IBOutlet UILabel* mnemonicLabel;
 }
 @end
 
@@ -30,9 +32,13 @@
     NSDictionary* s = [privKey addSignatureToJSON: obj expiresAfter: 5*60];
     NSData* json = [NSJSONSerialization dataWithJSONObject: s options: 0 error: nil];
     NSLog(@"\n%@", [[NSString alloc] initWithData: json encoding: NSUTF8StringEncoding]);
-    NSLog(@"Displaying QR code of %lu bytes (%lu bits) of data", json.length, 8*json.length);
+    NSLog(@"Displaying QR code of %lu bytes (%lu bits) of data",
+          (unsigned long)json.length, (unsigned long)8*json.length);
     UIImage* qrImage = [CBQRCode QRCodeImageWithData: json];
     qrDisplayView.image = qrImage;
+
+    mnemonicLabel.text = [privKey.publicKey.keyData my_mnemonicWithFormat: @"X X X\n"];
+    NSLog(@"Mnemonic:\n%@", mnemonicLabel.text);
 }
 
 @end
